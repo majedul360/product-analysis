20ac4b0a5598ad9b9cdff1588d71c5cd63e09a82
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-04-04 16:34:45 +0600

    hamburger menu added and responsive done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,54 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
   return (
     <div className="header">
-      <nav>
-        <Link to="/home" className="nav-link">
+      <nav className={`${menuIsOpen && "active"}`}>
+        <Link
+          onClick={() => setMenuIsOpen(false)}
+          to="/home"
+          className="nav-link"
+        >
           Home
         </Link>
-        <Link to="/reviews" className="nav-link">
+        <Link
+          onClick={() => setMenuIsOpen(false)}
+          to="/reviews"
+          className="nav-link"
+        >
           Reviews
         </Link>
-        <Link to="/dashboard" className="nav-link">
+        <Link
+          onClick={() => setMenuIsOpen(false)}
+          to="/dashboard"
+          className="nav-link"
+        >
           Dashboard
         </Link>
-        <Link to="/blogs" className="nav-link">
+        <Link
+          onClick={() => setMenuIsOpen(false)}
+          to="/blogs"
+          className="nav-link"
+        >
           Blogs
         </Link>
-        <Link to="/about" className="nav-link">
+        <Link
+          onClick={() => setMenuIsOpen(false)}
+          to="/about"
+          className="nav-link"
+        >
           About
         </Link>
       </nav>
+      <FontAwesomeIcon
+        onClick={() => setMenuIsOpen(!menuIsOpen)}
+        className="hambarger-menu"
+        icon={faBars}
+      />
     </div>
   );
 };
