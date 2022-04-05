5bef27a5ba0a306616009a69337cf8b938f78cf2
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-04-05 11:16:14 +0600

    question and answer section done

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -2,8 +2,32 @@ import "./Blogs.css";
 
 const Blogs = () => {
   return (
-    <div>
-      <h1>this is blog page</h1>
+    <div className="blogs">
+      <h3>1. What is context API?</h3>
+      <p>
+        Usually, data is passed from parent to children by props in react. When
+        data is passed from components to components by props then props have to
+        set every component. But context API provides us exchange or passing
+        data from components to components without prop drilling. So context API
+        is the structure that gives us power for exchanging or passing data
+        without props.
+      </p>
+      <h3>2. What is semantice element?</h3>
+      <p>
+        Semantic elements mean meaningful elements that humans and machines can
+        understand the purpose of elements and the content inside elements.
+        Elements such as Header, Footer, Article, and Section all are considered
+        semantic elements because they accurately describe the purpose of the
+        element and the content type that is inside them.
+      </p>
+      <h3>3. What is difference between inline and block element?</h3>
+      <p>
+        A block-level element always starts on a new line. An inline element
+        does not start on a new line. A block-level element takes up full width
+        available. An inline element takes up as much width as necessary.
+        Block-level element such as p, div, section etc and inline element such
+        as span, a, img etc.
+      </p>
     </div>
   );
 };
